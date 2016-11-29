5118b3ec953948930b7111dfd5bc9e14ba085a5d
Phil <plmok61@gmail.com>
2016-11-28 18:25:40 -0600

    adds bootstrap rows and cols

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
 import logo from './assets/galaxy-2.png'
 import './assets/App.css'
+import { Col } from 'react-bootstrap'
 import Equation from './Equation'
 import InfoBox from './InfoBox'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Drake Equation</h2>
-        </div>
-        <p className="App-intro">
-        How many alien civilizations are there right now?
-        </p>
-        <Equation />
-        <InfoBox />
+      <div bsClass='row' className='App'>
+        <Col xs={12} md={3} />
+        <Col xs={12} md={6}>
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Drake Equation</h2>
+          </div>
+          <p className="App-intro">
+          How many alien civilizations are there right now?
+          </p>
+          <Equation />
+          <InfoBox />
+        </Col>
+        <Col xs={12} md={3} />
       </div>
     );
   }
